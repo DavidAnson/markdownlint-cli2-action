29c40c5e62ad20742645412e3650e6fe85e2663f
David Anson <david@dlaa.me>
2021-01-12 22:51:40 -0800

    Stub markdownlint-cli2-formatter-default to verify functionality.

--- dist/index.js
+++ dist/index.js
@@ -12453,6 +12453,38 @@ module.exports = Token;
 
 /***/ }),
 
+/***/ 8552:
+/***/ ((module) => {
+
+"use strict";
+// @ts-check
+
+
+
+// Formats markdownlint-cli2 results in the style of `markdownlint-cli`
+const outputFormatter = (options) => {
+  const { results, logError } = options;
+  for (const errorInfo of results) {
+    const { fileName, lineNumber, ruleNames, ruleDescription, errorDetail,
+      errorContext, errorRange } = errorInfo;
+    const ruleName = ruleNames.join("/");
+    const description = ruleDescription +
+          (errorDetail ? ` [${errorDetail}]` : "") +
+          (errorContext ? ` [Context: "${errorContext}"]` : "");
+    const column = (errorRange && errorRange[0]) || 0;
+    const columnText = column ? `:${column}` : "";
+    logError(
+      `${fileName}:${lineNumber}${columnText} ${ruleName} ${description}`
+    );
+  }
+  return Promise.resolve();
+};
+
+module.exports = outputFormatter;
+
+
+/***/ }),
+
 /***/ 3112:
 /***/ ((module) => {
 
@@ -12929,10 +12961,11 @@ const outputSummary =
         logMessage,
         logError
       };
-      const formattersAndParams = requireIdsAndParams(
-        ".",
-        outputFormatters || [ [ "markdownlint-cli2-formatter-default" ] ]
-      );
+      // const formattersAndParams = requireIdsAndParams(
+      //   ".",
+      //   outputFormatters || [ [ "markdownlint-cli2-formatter-default" ] ]
+      // );
+      const formattersAndParams = [ [ __nccwpck_require__(8552) ] ];
       await Promise.all(formattersAndParams.map((formatterAndParams) => {
         const [ formatter, ...formatterParams ] = formatterAndParams;
         return formatter(formatterOptions, ...formatterParams);
