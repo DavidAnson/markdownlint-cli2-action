0ec86e944ca4978984a7ee888ee9b9d47b3203ff
David Anson <david@dlaa.me>
2026-06-18 20:28:02 -0700

    Freshen generated index.js file.

--- dist/index.mjs
+++ dist/index.mjs
@@ -77234,7 +77234,7 @@ function charFromCodepoint(c) {
     return String.fromCharCode(c);
   }
   // Encode UTF-16 surrogate pair
-  // https://wikipedia.org/wiki/UTF-16#Code_points_U.2B010000_to_U.2B10FFFF
+  // https://en.wikipedia.org/wiki/UTF-16#Code_points_U.2B010000_to_U.2B10FFFF
   return String.fromCharCode(
     ((c - 0x010000) >> 10) + 0xD800,
     ((c - 0x010000) & 0x03FF) + 0xDC00
